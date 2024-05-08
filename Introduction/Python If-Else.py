05a7589065ae3c7c0741064e887a8f9365b4ffcc
Rajendra Patil <rajendrapatil9673@gmail.com>
2024-05-08 10:17:10 +0530

    Create Python If-Else.py

---
+++ Introduction/Python If-Else.py
@@ -0,0 +1,23 @@
+#github.com/rajendra-patil96
+#Python/Introduction/Python If-Else.py
+
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    if(n%2!=0):
+        print("Weird")
+    elif(n%2==0 and (n>=2 and n<=5)):
+        print("Not Weird")
+    elif(n%2==0 and (n>=6 and n<=20)):
+        print("Weird")
+    else:
+        print("Not Weird")
+
