b7ccd423315709afe4542eda1c5137d6d99e1f14
Rajendra Patil <rajendrapatil9673@gmail.com>
2024-05-18 15:13:41 +0530

    Create List Comprehensions.py

---
+++ Basic Data Types/List Comprehensions.py
@@ -0,0 +1,17 @@
+#github.com/rajendra-patil96
+#Python/Basic Data Types/List Comprehensions.py
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    result = []
+    for i in range(0,x+1):
+        for j in range(0,y+1):
+            for k in range(0,z+1):
+                if(i+j+k != n):
+                    result.append([i,j,k])
+    
+    print(result)
